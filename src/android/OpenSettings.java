b9561aec99aeddbc0cb10b27f65b980f220ec0bf
Erik Huisman <erik@innovatio.nl>
2015-01-16 10:48:06 +0100

    Java version of bluetooth settings

--- src/android/OpenSettings.java
+++ src/android/OpenSettings.java
@@ -1,29 +1,37 @@
 package nl.tapme.cordova.opensettings;
 
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.util.Log;
-import org.apache.cordova.*;
-import org.apache.cordova.api.*;  // for Cordova 2.9
-import org.json.JSONArray;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaArgs;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.PluginResult;
+
 import org.json.JSONException;
+import org.json.JSONArray;
 import org.json.JSONObject;
+
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.bluetooth.BluetoothAdapter;
 
 public class OpenSettings extends CordovaPlugin {
 
 	private Context context;
+	private BluetoothAdapter _adapter;
 
 	@Override
-	public void initialize( CordovaInterface cordova, CordovaWebView webView ) {
-		super.initialize( cordova, webView );
+    public void initialize(final CordovaInterface cordova, CordovaWebView webView) {
+        Log.v(TAG, "OpenSettings: initialization");
+        super.initialize(cordova, webView);
+		_adapter = BluetoothAdapter.getDefaultAdapter();
+    }
 
-		this.context = cordova.getActivity().getApplicationContext();
-	}
-
-	public boolean execute( String action, JSONArray data, CallbackContext callbackContext ) throws JSONException {
+    public boolean execute(String action, CordovaArgs args, final CallbackContext callbackContext) throws JSONException {
 
 		if( action.equals( "bluetoothStatus" ) ) {
-			callbackContext.success('hi from android');
+			Log.v(TAG, "OpenSettings: bluetoothStatus");
+			callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, _adapter.getState()));
 			return true;
 		}
 

3680e074dbe3d76315979a10f154207490732711
Erik Huisman <erik@innovatio.nl>
2015-01-18 03:18:36 +0100

    bluetooth state change listener

--- www/OpenSettings.js
+++ www/OpenSettings.js
@@ -5,13 +5,13 @@ OpenSettings.settings = function(app, callback) {
 		// Success callback
 		callback,
 		// Failure callback
-		function(err) { console.log('Missing app scheme.');},
+		function(err) { console.log('OpenSettins.settings error'); },
 		// Native Class Name
 		"OpenSettings",
 		// Name of method in native class.
 		"settings",
 		// array of args to pass to method.
-		[app]
+		[]
 	);
 };
 
@@ -20,13 +20,13 @@ OpenSettings.bluetooth = function (app, callback) {
 		// Success callback
 		callback,
 		// Failure callback
-		function(err) { console.log('Missing app scheme.');},
+		function(err) { console.log('OpenSettins.bluetooth error'); },
 		// Native Class Name
 		"OpenSettings",
 		// Name of method in native class.
 		"bluetooth",
 		// array of args to pass to method.
-		[app]
+		[]
 	);
 };
 
@@ -35,13 +35,28 @@ OpenSettings.bluetoothStatus = function (app, callback) {
 		// Success callback
 		callback,
 		// Failure callback
-		function(err) { console.log('Missing app scheme.');},
+		function(err) { console.log('OpenSettins.bluetoothStatus error'); },
 		// Native Class Name
 		"OpenSettings",
 		// Name of method in native class.
 		"bluetoothStatus",
 		// array of args to pass to method.
-		[app]
+		[]
+	);
+};
+
+OpenSettings.bluetoothChange = function (callback) {
+	cordova.exec(
+		// Success callback
+		callback,
+		// Failure callback
+		function(err) { console.log('OpenSettins.bluetoothChange error'); },
+		// Native Class Name
+		"OpenSettings",
+		// Name of method in native class.
+		"bluetoothChange",
+		// array of args to pass to method.
+		[]
 	);
 };
 
